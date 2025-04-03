4611c93e5c473f2150cdc42581309380e69f2977
MG <poolhanger77@gmail.com>
2025-04-03 19:01:30 +0000

    simple sound sysetem

--- src/main/java/org/ginafro/notenoughfakepixel/utils/SoundUtils.java
+++ src/main/java/org/ginafro/notenoughfakepixel/utils/SoundUtils.java
@@ -3,41 +3,31 @@ package org.ginafro.notenoughfakepixel.utils;
 import net.minecraft.client.Minecraft;
 import net.minecraft.util.BlockPos;
 import net.minecraft.util.EnumChatFormatting;
-import net.minecraft.util.ResourceLocation;
 import org.ginafro.notenoughfakepixel.NotEnoughFakepixel;
 
 public class SoundUtils {
 
     public static void playSound(int[] cords, String sound, float volume, float pitch) {
-        playSound(cords, sound, volume, pitch, 1, 0);
+        playSound(cords[0], cords[1], cords[2], sound, volume, pitch);
     }
 
     public static void playSound(BlockPos pos, String sound, float volume, float pitch) {
-        int[] position = new int[]{pos.getX(), pos.getY(), pos.getZ()};
-        playSound(position, sound, volume, pitch, 1, 0);
+        playSound(pos.getX(), pos.getY(), pos.getZ(), sound, volume, pitch);
     }
 
     public static void playSound(float x, float y, float z, String sound, float volume, float pitch) {
         if (!NotEnoughFakepixel.feature.misc.enableSounds) return;
+
+        Minecraft mc = Minecraft.getMinecraft();
+        if (mc.thePlayer == null) return;
+
         try {
-            ResourceLocation res = new ResourceLocation(sound);
-            if (Minecraft.getMinecraft().getSoundHandler() == null) {
-                return;
-            }
             if (NotEnoughFakepixel.feature.debug.debug && NotEnoughFakepixel.feature.debug.showSounds) {
                 Logger.log(EnumChatFormatting.WHITE + "Playing sound: " + sound + " at " + x + ", " + y + ", " + z + " with volume " + volume + " and pitch " + pitch);
             }
 
-            Minecraft.getMinecraft().getSoundHandler().playSound(
-                    new net.minecraft.client.audio.PositionedSoundRecord(
-                            res,
-                            volume,
-                            pitch,
-                            x,
-                            y,
-                            z
-                    )
-            );
+            mc.thePlayer.playSound(sound, volume, pitch);
+
         } catch (Exception e) {
             if (NotEnoughFakepixel.feature.debug.debug) {
                 Logger.log(EnumChatFormatting.RED + "Failed to play sound: " + sound + ". Error " + e.getMessage());
@@ -48,10 +38,12 @@ public class SoundUtils {
     public static void playSound(int[] cords, String sound, float volume, float pitch, int times, int delay) {
         for (int i = 0; i < times; i++) {
             playSound(cords[0], cords[1], cords[2], sound, volume, pitch);
-            try {
-                Thread.sleep(delay);
-            } catch (InterruptedException e) {
-                // ignored
+            if (delay > 0) {
+                try {
+                    Thread.sleep(delay);
+                } catch (InterruptedException e) {
+                    // ignored
+                }
             }
         }
     }
