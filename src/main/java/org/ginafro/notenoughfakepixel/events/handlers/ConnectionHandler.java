30277b86965b5b5809cd9047d220547d341388d4
David <dabelesp2@gmail.com>
2025-05-13 08:29:53 +0200

    Log error

--- src/main/java/org/ginafro/notenoughfakepixel/events/handlers/ConnectionHandler.java
+++ src/main/java/org/ginafro/notenoughfakepixel/events/handlers/ConnectionHandler.java
@@ -4,6 +4,7 @@ import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.common.network.FMLNetworkEvent;
 import org.ginafro.notenoughfakepixel.envcheck.registers.RegisterEvents;
 import org.ginafro.notenoughfakepixel.events.NEFClientConnectedToServerEvent;
+import org.ginafro.notenoughfakepixel.utils.Logger;
 
 import java.net.InetSocketAddress;
 import java.net.SocketAddress;
@@ -17,7 +18,7 @@ public class ConnectionHandler {
     public void onServerConnect(FMLNetworkEvent.ClientConnectedToServerEvent event) {
         NEFClientConnectedToServerEvent nefEvent = NEFClientConnectedToServerEvent.create(event);
         event.manager.channel().pipeline().addBefore("packet_handler", "nef_packet_handler", new PacketHandler());
-        System.out.println("Added packet handler to channel pipeline.");
+        Logger.logConsole("Added packet handler to channel pipeline.");
         SocketAddress address = nefEvent.manager.getRemoteAddress();
 
         if (address instanceof InetSocketAddress) {
@@ -27,9 +28,9 @@ public class ConnectionHandler {
 
             if (hostName.contains(TARGET_SERVER)) {
                 ApiHandler.init();
-                System.out.println("Connected to " + TARGET_SERVER + ":" + port);
+                Logger.logConsole("Connected to " + TARGET_SERVER + ":" + port);
             } else {
-                System.out.println("Not connected to " + TARGET_SERVER + ":" + port);
+                Logger.logConsole("Not connected to " + TARGET_SERVER + ":" + port);
             }
         }
 
